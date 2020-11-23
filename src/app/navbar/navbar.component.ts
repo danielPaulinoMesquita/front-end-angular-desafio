7eef63bd0509c561979974223dc53d74c3351bf0
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2020-11-23 14:25:11 -0300

    3- Diretórios que não haviam subido para o repo.

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styles: ['node_modules/bootstrap/dist/css/bootstrap.min.css',
+  'styles.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+
+}
